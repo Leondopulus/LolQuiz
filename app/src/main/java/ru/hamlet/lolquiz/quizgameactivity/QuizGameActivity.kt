fb65738f719ab575218f9c4be1cdfa95fb7727b6
N. Frantusov <francuzov72@gmail.com>
2022-07-14 16:45:19 +0200

    Refactoring

--- app/src/main/java/ru/hamlet/lolquiz/quizgameactivity/QuizGameActivity.kt
+++ app/src/main/java/ru/hamlet/lolquiz/quizgameactivity/QuizGameActivity.kt
@@ -9,177 +9,134 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat.startDragAndDrop
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import ru.hamlet.lolquiz.LolItem
 import ru.hamlet.lolquiz.MyDragShadowBuilder
 import ru.hamlet.lolquiz.R
-import kotlin.random.Random
-
-import android.graphics.Canvas
-import android.graphics.Point
-import android.view.DragEvent.ACTION_DRAG_ENTERED
-import android.view.DragEvent.ACTION_DROP
-import androidx.core.content.res.ResourcesCompat
-
 
 
 class QuizGameActivity() : AppCompatActivity() {
 
-    val viewModel: QuizViewModel by viewModel()
-//    val MyDragShadowBuilder: MyDragShadowBuilder()
-
-    val listVariables = mutableListOf<ImageView>()
-
+    private val viewModel: QuizViewModel by viewModel()
 
     //////////////////////////////////////////////////
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_game)
 
         viewModel.onCreate()
 
-
         findViewById<Button>(R.id.okButton).setOnClickListener {
-            println("2")
             viewModel.onOkButtonClick()
         }
 
-
         viewModel.questItemLiveData.observe(this) { lolItem ->
-            if (lolItem == null) {
-                viewModel.getNewQuizItem()
-            } else {
-                setQuestItem(lolItem)
-            }
-
+            setQuestItem(lolItem)
         }
 
-
-//        findViewById<View>(R.id.componentImg0).setOnDragListener()
-
-        val imgView = findViewById<ImageView>(R.id.img0)
-        setDragAndDropOnImageView(imgView)
-
-
-        findViewById<View>(R.id.img1).setOnClickListener {
-            println("long click")
-        }
-        findViewById<View>(R.id.img2).setOnClickListener {
-            viewModel.onImgButtonClick(findViewById<View>(R.id.img2))
-        }
-        findViewById<View>(R.id.img3).setOnClickListener {
-            viewModel.onImgButtonClick(findViewById<View>(R.id.img3))
-        }
-        findViewById<View>(R.id.img4).setOnClickListener {
-            viewModel.onImgButtonClick(findViewById<View>(R.id.img4))
-        }
-        findViewById<View>(R.id.img5).setOnClickListener {
-            viewModel.onImgButtonClick(findViewById<View>(R.id.img5))
-        }
-        findViewById<View>(R.id.img6).setOnClickListener {
-            viewModel.onImgButtonClick(findViewById<View>(R.id.img6))
-        }
-        findViewById<View>(R.id.img7).setOnClickListener {
-            viewModel.onImgButtonClick(findViewById<View>(R.id.img7))
-        }
-        findViewById<View>(R.id.img8).setOnClickListener {
-            viewModel.onImgButtonClick(findViewById<View>(R.id.img8))
-        }
-        findViewById<View>(R.id.img9).setOnClickListener {
-            viewModel.onImgButtonClick(findViewById<View>(R.id.img9))
+        viewModel.levelComponentVariantsLiveData.observe(this) { variants ->
+            displayItemVariants(variants)
         }
 
-        // 1
-        val viewDragListener = View.OnDragListener { view, dragEvent ->
-
-            //2
-            val draggableItem = dragEvent.localState as View
-
-            //3
-            when (dragEvent.action) {
-                DragEvent.ACTION_DRAG_STARTED -> {
-                    println("draggg started ")
-                    true
-                }
-                DragEvent.ACTION_DRAG_ENTERED -> {
-                    println("draggg entered ")
-                    view.invalidate()
-                    true
-                }
-                DragEvent.ACTION_DRAG_LOCATION -> {
-                    println("draggg location? wtf is tis? ")
-                    true
-                }
-                DragEvent.ACTION_DRAG_EXITED -> {
-                    println("draggg exited ")
-                    true
-                }
-                DragEvent.ACTION_DROP -> {
-                    println("draggg dropped ")
-                    true
-                }
-                DragEvent.ACTION_DRAG_ENDED -> {
-                    println("draggg ended ")
-                    true
-                }
-                else -> {
-                    println("draggg falsed ")
-                    false
-                }
-            }
-        }
-
-        val slot = mutableListOf<ImageView>(
+        val slotsImageViews = mutableListOf<ImageView>(
             findViewById(R.id.componentImg0),
             findViewById(R.id.componentImg1),
             findViewById(R.id.componentImg2)
         )
-        //slot[0].setOnDragListener(viewDragListener)
+        viewModel.slotsLiveData.observe(this) { slots ->
+            for (i in 0 until slotsImageViews.size) {
+                val slotImage = slotsImageViews[i]
+                if (i >= slots.size) {
+                    slotImage.visibility = View.GONE
+                } else {
+                    slotImage.visibility = View.VISIBLE
 
-        var view = findViewById<ImageView>(R.id.img0)
-        listVariables.add(view)
-        view.setOnDragListener(viewDragListener)
-
-        view = findViewById(R.id.img1)
-        listVariables.add(view)
-        view = findViewById(R.id.img2)
-        listVariables.add(view)
-        view = findViewById(R.id.img3)
-        listVariables.add(view)
-        view = findViewById(R.id.img4)
-        listVariables.add(view)
-        view = findViewById(R.id.img5)
-        listVariables.add(view)
-        view = findViewById(R.id.img6)
-        listVariables.add(view)
-        view = findViewById(R.id.img7)
-        listVariables.add(view)
-        view = findViewById(R.id.img8)
-        listVariables.add(view)
-        view = findViewById(R.id.img9)
-        listVariables.add(view)
-
-
-
-
+                    val slotData = slots[i]
+                    if (slotData == null) {
+                        slotImage.setImageResource(R.drawable.frame)
+                    } else {
+                        slotImage.displayLolItemImage(slotData)
+                    }
+                }
+            }
+        }
 
     }
 
+    private fun displayItemVariants(variants: List<LolItem>) {
+        for (i in variants.indices) {
+            val lolItem = variants[i]
 
-    fun setDragAndDropOnImageView (view: ImageView){
-        ///////////////////////////////////////////
-        // Create a string for the ImageView label.
-        val IMAGEVIEW_TAG = "icon bitmap"
-        val iconBitmap = Bitmap.createBitmap(100, 100,
-        Bitmap.Config.ARGB_8888)
+            val id = resources.getIdentifier("img${i}", "id", packageName)
+            val variantImg = findViewById<ImageView>(id)
+            variantImg.displayLolItemImage(lolItem)
 
-        view.apply {
-            // Sets the bitmap for the ImageView from an icon bit map (defined elsewhere).
-            setImageBitmap(iconBitmap)
-            tag = IMAGEVIEW_TAG
+            // TODO make one function 'addDragSupport' hz
+            variantImg.setLongClickListenerForDragAndDrop(variantIndex = i, lolItem = lolItem);
+            variantImg.setDragListenerForVariant(lolItem)
+        }
+    }
+
+    private fun ImageView.setDragListenerForVariant(lolItem: LolItem) {
+        setOnDragListener { view, dragEvent ->
+//  TODO make proper drag event handling for image variant
+//
+//
+//            val draggableItem = dragEvent.localState as View
+//
+//            //3
+//            when (dragEvent.action) {
+//                DragEvent.ACTION_DRAG_STARTED -> {
+//                    println("draggg started ")
+//                    true
+//                }
+//                DragEvent.ACTION_DRAG_ENTERED -> {
+//                    println("draggg entered ")
+//                    view.invalidate()
+//                    true
+//                }
+//                DragEvent.ACTION_DRAG_LOCATION -> {
+//                    println("draggg location? wtf is tis? ")
+//                    true
+//                }
+//                DragEvent.ACTION_DRAG_EXITED -> {
+//                    println("draggg exited ")
+//                    true
+//                }
+//                DragEvent.ACTION_DROP -> {
+//                    println("draggg dropped ")
+//                    true
+//                }
+//                DragEvent.ACTION_DRAG_ENDED -> {
+//                    println("draggg ended ")
+//                    true
+//                }
+//                else -> {
+//                    println("draggg falsed ")
+//                    false
+//                }
+//            }
+//        }
+            true
+        }
+
+    }
+
+    private fun ImageView.displayLolItemImage(item: LolItem) {
+        Glide.with(this)
+            .applyDefaultRequestOptions(
+                RequestOptions()
+                    .placeholder(R.drawable.item0)
+//                    .error(R.drawable.ic_user_default)
+            )
+            .load(item.imageUrl).into(this)
+    }
+
+    private fun ImageView.setLongClickListenerForDragAndDrop(variantIndex: Int, lolItem: LolItem) {
+        this.apply {
+            tag = lolItem.id
             setOnLongClickListener { v ->
                 // Create a new ClipData.
                 // This is done in two steps to provide clarity. The convenience method
@@ -192,7 +149,7 @@ class QuizGameActivity() : AppCompatActivity() {
                 // the already-created item. This creates a new ClipDescription object within the
                 // ClipData and sets its MIME type to "text/plain".
                 val dragData = ClipData(
-                    v.tag as? CharSequence,
+                    v.tag as? CharSequence, // TODO add data about variant image we are dragging
                     arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN),
                     item)
 
@@ -212,98 +169,11 @@ class QuizGameActivity() : AppCompatActivity() {
         }
     }
 
-    ///////////////////////////////////////////////////
-//    @SuppressLint("CutPasteId")
-    fun setQuestItem(lolItem: LolItem) {
+    private fun setQuestItem(lolItem: LolItem) {
         println("debug setQuestItem start")
         val questImg = findViewById<ImageView>(R.id.questImg)
-        Glide.with(this).load(lolItem.imageUrl).into(questImg)
-
-        ////////////////////
-        var rIndex = 0
-        val indexSet = mutableSetOf<Int>()
-        val finalIdexSet = mutableSetOf<Int>()
-        for (i in 0..(listVariables.size - 1)) {
-            finalIdexSet.add(i)
-        }
-
-
-        //////////////////
-        val slot = mutableListOf<ImageView>(
-            findViewById(R.id.componentImg0),
-            findViewById(R.id.componentImg1),
-            findViewById(R.id.componentImg2)
-        )
-//        slot[0].setOnDragListener()
-
-        val lvl1components = lolItem.lvl1Components
-        println("lvl1components: " + lvl1components)
-
-
-        //add true items
-        var itemId: Int
-        var item: LolItem?
-        var img: View
-
-        for (j in 0 until slot.size) {
-            if (lvl1components.size > j) {
-
-                slot[j].visibility = View.VISIBLE //
-
-                while (true) {
-                    rIndex = (0..(listVariables.size - 1)).random()
-                    if (indexSet.contains(rIndex)) {
-                        continue
-                    } else {
-                        itemId = lvl1components[j]
-                        item = viewModel.getItem(itemId)
-                        img = listVariables.get(rIndex)
-                        if (item != null) {
-                            Glide.with(this).load(item.imageUrl).into(img)
-                            println("item " + item.name + " pos " + rIndex)
-                        }
-                        indexSet.add(rIndex)
-                        break
-                    }
-                }
-            } else {
-                slot[j].visibility = View.GONE    //
-            }
-        }
-
-        //add random items
-        while (true) {
-            rIndex = (0..(listVariables.size - 1)).random()
-            if (indexSet == finalIdexSet) {
-                break
-            } else if (indexSet.contains(rIndex)) {
-                continue
-            } else {
-                img = listVariables.get(rIndex)
-                item = null
-
-                while (item == null) {
-                    val randomItem = viewModel.getItem()
-                    if (randomItem.lvl1Components.size != 0) {
-                        item =
-                            viewModel.getItem(randomItem.lvl1Components.get((0..(randomItem.lvl1Components.size - 1)).random()))
-                    }
-                }
-                Glide.with(this).load(item.imageUrl).into(img)
-                indexSet.add(rIndex)
-                continue
-            }
-        }
+        questImg.displayLolItemImage(lolItem)
     }
 }
-
-class MyDragListener: View.OnDragListener{
-    override fun onDrag(p0: View?, p1: DragEvent?): Boolean {
-        TODO("Not yet implemented")
-    }
-
-}
-
-
 
 
--- app/src/main/java/ru/hamlet/lolquiz/quizgameactivity/QuizViewModel.kt
+++ app/src/main/java/ru/hamlet/lolquiz/quizgameactivity/QuizViewModel.kt
@@ -1,7 +1,5 @@
 package ru.hamlet.lolquiz.quizgameactivity
 
-import android.content.ClipData
-import android.content.ClipDescription
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
@@ -12,21 +10,22 @@ import kotlinx.coroutines.withContext
 import ru.hamlet.lolquiz.LolItem
 import ru.hamlet.lolquiz.listlolitems.LolItemsRepository
 import androidx.lifecycle.viewModelScope
+import com.bumptech.glide.Glide
 import kotlinx.coroutines.launch
-import ru.hamlet.lolquiz.MyDragShadowBuilder
+import ru.hamlet.lolquiz.R
+import java.lang.IllegalStateException
+
 
 class QuizViewModel(val repository: LolItemsRepository) : ViewModel() {
 
-    var listLolItems = MutableLiveData<List<LolItem>>()
-    var questItemLiveData = MutableLiveData<LolItem?>()
+    private val LOG_TAG = "QuizViewModel"
 
-    var item0IDLiveData = MutableLiveData<Int>()
-    var item1IDLiveData = MutableLiveData<Int>()
-    var item2IDLiveData = MutableLiveData<Int>()
+    private var lolItems = listOf<LolItem>();
 
-    var img0LiveData = MutableLiveData<Int>()
+    var questItemLiveData = MutableLiveData<LolItem>()
+    var slotsLiveData = MutableLiveData<List<LolItem?>>()
+    var levelComponentVariantsLiveData = MutableLiveData<List<LolItem>>()
 
-    var itemComponentsIDs = mutableListOf<MutableLiveData<Int>>()
 
     ////////////////////////////////////
     fun onCreate() {
@@ -34,111 +33,109 @@ class QuizViewModel(val repository: LolItemsRepository) : ViewModel() {
             val result = withContext(Dispatchers.IO) {
                 //IO
                 val result: List<LolItem>? = repository.getItems() //getItemsRequestOrNull()
-                Log.d("ViewModel", result.toString())
+                Log.d(LOG_TAG, result.toString())
 
                 return@withContext result
             }
-            val repositoryList: List<LolItem> = if (result != null) {
-                result
-            } else {
-                listOf()
-            }
-            itemComponentsIDs.add(item0IDLiveData)
-            itemComponentsIDs.add(item1IDLiveData)
-            itemComponentsIDs.add(item2IDLiveData)
+            val repositoryList = result ?: listOf()
+            lolItems = repositoryList
 
-
-            println("view model got result")
-            listLolItems.value = repositoryList
-            getNewQuizItem()
-
-
+            Log.d(LOG_TAG, "Got result")
+            createNewQuizQuestion()
         }
-
-
-
-
     }
 
     fun onOkButtonClick() {
-        if(checkIsItemValid() == true){
-            Log.d("quizresult", "its true")
+        if(checkIsItemValid()){
+            Log.d(LOG_TAG, "its true")
         }else{
-            Log.d("quizresult", "its false")
+            Log.d(LOG_TAG, "its false")
         }
 
-        getNewQuizItem()
-    }
-
-    fun onImgButtonClick(button : View){
-
+        createNewQuizQuestion()
     }
 
     fun checkIsItemValid(): Boolean {
-        val item = questItemLiveData.value
-        val setOfAnswerIds = mutableSetOf<Int>()
-
-        for (i in 0 until itemComponentsIDs.size){
-            if (itemComponentsIDs[i].value != null){
-                setOfAnswerIds.add(itemComponentsIDs[i].value!!)
-            }
-        }
-
-        val quizIdsSet = mutableSetOf<Int>()
-        if (item != null) {
-            for (i in 0 until item.lvl1Components.size)
-                quizIdsSet.add(item.lvl1Components[i])
-        }
-
-        if (setOfAnswerIds == quizIdsSet){
-            return true
-        }
-
-        return false
+        // TODO check slotsLiveData.value to validate is answer right or not!
+        return true
+//        val item = questItemLiveData.value
+//        val setOfAnswerIds = mutableSetOf<Int>()
+//
+//        for (i in 0 until slotsLiveData.va.size){
+//            if (itemComponentsIDs[i].value != null){
+//                setOfAnswerIds.add(itemComponentsIDs[i].value!!)
+//            }
+//        }
+//
+//        val quizIdsSet = mutableSetOf<Int>()
+//        if (item != null) {
+//            for (i in 0 until item.lvl1Components.size)
+//                quizIdsSet.add(item.lvl1Components[i])
+//        }
+//
+//        if (setOfAnswerIds == quizIdsSet){
+//            return true
+//        }
+//
+//        return false
     }
 
-    fun getNewQuizItem() {
-        while (true){
-            val item = getItem()
-            if (item.lvl1Components.isEmpty()) {
-                Log.d("quizVM", "lvl1Comp = empty")
-                continue
-            } else{
-                Log.d("quizVM", "comps: " + item.lvl1Components)
-                questItemLiveData.value = item
-                break
-            }
+    private fun createNewQuizQuestion() {
+        val randomLolItem = getRandomItemWithComponents()
+        questItemLiveData.value = randomLolItem
+        levelComponentVariantsLiveData.value = generateQuestVariants(randomLolItem)
+        slotsLiveData.value = List(randomLolItem.lvl1Components.size) { null }
+    }
+
+    private fun generateQuestVariants(questItem: LolItem): List<LolItem> {
+        val lvl1components = questItem.lvl1Components
+        Log.d(LOG_TAG, "lvl1components: $lvl1components")
+
+        val result = mutableListOf<LolItem>()
+        for (componentId in lvl1components) {
+            val component = getLolItemOrNull(componentId) ?: throw IllegalStateException("For some reason we can't find lol item with id[$componentId]")
+            result.add(component)
+        }
+
+        val variantsCount = 10
+        val remainingCount = variantsCount - result.size
+
+        for (i in 0 until remainingCount) {
+            // TODO add logic to respect 'isItemHasNoParents'
+            val randomItem = getRandomItem(excludingIds = listOf(questItem.id))
+            result.add(randomItem)
+        }
+
+        return result.shuffled()
+    }
+
+    private fun getRandomItemWithComponents(): LolItem {
+        while (true) {
+            val item = getRandomItem()
+            if (item.lvl1Components.isEmpty()) continue
+            return item
         }
     }
 
-    fun getItem(): LolItem {
-
-        val listItems = listLolItems.value
-        Log.d("quizVM", "1")
-        var item: LolItem? = null
-
-        while (item == null){
-            item = listItems?.get((0..(listItems.size - 1)).random())
-            Log.d("quizVM", "item: "+ item)
-
+    private fun getRandomItem(excludingIds: List<Int> = listOf()): LolItem {
+        while (true) {
+            val item = lolItems.random()
+            if (excludingIds.contains(item.id)) continue
+            return item
         }
-
-        return item
     }
 
-    fun getItem(itemId: Int): LolItem? {
-        val listItems = listLolItems.value
-        if (listItems != null) {
-            for (i in 0..(listItems.size - 1)) {
-                val item = listItems.get(i)
-                if (item.id == itemId) {
-                    return item
-
-                }
-            }
+    private fun getLolItemOrNull(itemId: Int): LolItem? {
+        for (item in lolItems) {
+            if (item.id == itemId) return item;
         }
         return null
     }
 
+
+    fun isItemHasNoParents(itemId: Int): Boolean {
+        // TODO add implementation
+        return false
+    }
 
 }
